cd42046e8970b343f4999f6e63a337a989778fb0
Diego Barreiro <diego.barreiro.perez@gmail.com>
2020-06-23 20:23:54 +0200

    Remove unnecessary change

--- appinventor/buildserver/src/com/google/appinventor/buildserver/Compiler.java
+++ appinventor/buildserver/src/com/google/appinventor/buildserver/Compiler.java
@@ -623,24 +623,24 @@ public final class Compiler {
     out.write("\" parent=\"");
     out.write(parent);
     out.write("\">\n");
-    out.write("<item type=\"attr\" name=\"colorPrimary\">@color/colorPrimary</item>\n");
-    out.write("<item type=\"attr\" name=\"colorPrimaryDark\">@color/colorPrimaryDark</item>\n");
-    out.write("<item type=\"attr\" name=\"colorAccent\">@color/colorAccent</item>\n");
+    out.write("<item name=\"colorPrimary\">@color/colorPrimary</item>\n");
+    out.write("<item name=\"colorPrimaryDark\">@color/colorPrimaryDark</item>\n");
+    out.write("<item name=\"colorAccent\">@color/colorAccent</item>\n");
     boolean holo = sdk >= 11 && sdk < 21;
     boolean needsClassicSwitch = false;
     if (!parent.equals("android:Theme")) {
-      out.write("<item type=\"attr\" name=\"windowActionBar\">true</item>\n");
+      out.write("<item name=\"windowActionBar\">true</item>\n");
       out.write("<item name=\"android:windowActionBar\">true</item>\n");  // Honeycomb ActionBar
       if (parent.contains("Holo") || holo) {
         out.write("<item name=\"android:actionBarStyle\">@style/AIActionBar</item>\n");
-        out.write("<item type=\"attr\" name=\"actionBarStyle\">@style/AIActionBar</item>\n");
+        out.write("<item name=\"actionBarStyle\">@style/AIActionBar</item>\n");
       }
       // Handles theme for Notifier
       out.write("<item name=\"android:dialogTheme\">@style/AIDialog</item>\n");
       out.write("<item name=\"dialogTheme\">@style/AIDialog</item>\n");
       out.write("<item name=\"android:cacheColorHint\">#000</item>\n");  // Fixes crash in ListPickerActivity
     } else {
-      out.write("<item type=\"attr\" name=\"switchStyle\">@style/ClassicSwitch</item>\n");
+      out.write("<item name=\"switchStyle\">@style/ClassicSwitch</item>\n");
       needsClassicSwitch = true;
     }
     out.write("</style>\n");
@@ -676,9 +676,9 @@ public final class Compiler {
     out.write("\" parent=\"");
     out.write(parent);
     out.write("\">\n");
-    out.write("<item type=\"attr\" name=\"colorPrimary\">@color/colorPrimary</item>\n");
-    out.write("<item type=\"attr\" name=\"colorPrimaryDark\">@color/colorPrimaryDark</item>\n");
-    out.write("<item type=\"attr\" name=\"colorAccent\">@color/colorAccent</item>\n");
+    out.write("<item name=\"colorPrimary\">@color/colorPrimary</item>\n");
+    out.write("<item name=\"colorPrimaryDark\">@color/colorPrimaryDark</item>\n");
+    out.write("<item name=\"colorAccent\">@color/colorAccent</item>\n");
     if (parent.contains("Holo")) {
       // workaround for weird window border effect
       out.write("<item name=\"android:windowBackground\">@android:color/transparent</item>\n");
@@ -731,13 +731,13 @@ public final class Compiler {
       BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(colorsXml), "UTF-8"));
       out.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
       out.write("<resources>\n");
-      out.write("<color type=\"attr\" name=\"colorPrimary\">");
+      out.write("<color name=\"colorPrimary\">");
       out.write(colorPrimary);
       out.write("</color>\n");
-      out.write("<color type=\"attr\" name=\"colorPrimaryDark\">");
+      out.write("<color name=\"colorPrimaryDark\">");
       out.write(colorPrimaryDark);
       out.write("</color>\n");
-      out.write("<color type=\"attr\" name=\"colorAccent\">");
+      out.write("<color name=\"colorAccent\">");
       out.write(colorAccent);
       out.write("</color>\n");
       out.write("</resources>\n");
@@ -765,7 +765,7 @@ public final class Compiler {
         }
       }
       out.write("<style name=\"TextAppearance.AppCompat.Button\">\n");
-      out.write("<item type=\"attr\" name=\"textAllCaps\">false</item>\n");
+      out.write("<item name=\"textAllCaps\">false</item>\n");
       out.write("</style>\n");
       out.write("</resources>\n");
       out.close();
